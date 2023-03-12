ba65d5f664ebd16dd2fcb3525fee7bbeed4dc4e7
timtoronto634 <tim225x@gmail.com>
2023-03-12 11:28:21 +0900

    Feat use io writer for printing

--- cmd/population.go
+++ cmd/population.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -17,7 +19,8 @@ var populationCmd = &cobra.Command{
 	default is all, Tokyo, 1980-2020`,
 	Run: func(cmd *cobra.Command, args []string) {
 		for y := 1980; y <= 2020; y++ {
-			fmt.Printf("%v,%v,%d\n", "東京都", y, 1000000)
+			str := fmt.Sprintf("%v,%v,%d\n", "東京都", y, 1000000)
+			io.WriteString(os.Stdout, str)
 		}
 	},
 }
